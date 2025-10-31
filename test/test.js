002bb0e19672786514d2ab3833d76c2448ec01cb
OukSreySor <ouk.sreysor11@gmail.com>
2025-10-31 11:15:19 +0700

    Update greeting message

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ import { expect } from "chai";
 import app from "../app.js";
 
 describe('GET /', () => {
-    it('should return Hello, GitHub Actions!', async () => {
+    it('should return Hello, CI/CD!', async () => {
         const res = await request(app).get('/');
 
         expect(res.status).to.equal(200);
-        expect(res.text).to.equal('Hello, GitHub Actions!');
+        expect(res.text).to.equal('Hello, CI/CD!');
     });
 });
